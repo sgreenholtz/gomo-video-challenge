0671b9483d1149818b4f4147a3bf08581090c5ca
Sebastian Greenholtz <smgreenholtz@gmail.com>
2019-06-09 16:12:08 -0500

    Console text added

--- uvt-calculator/src/main/java/com/greenholtz/gomo/uvt/launcher/Launcher.java
+++ uvt-calculator/src/main/java/com/greenholtz/gomo/uvt/launcher/Launcher.java
@@ -17,13 +17,21 @@ public class Launcher {
 	 */
 	public static void main(String[] args) {
 		if (args.length==0 || args[0].equals("help")) {
-			// insert the explanation text here
+			StringBuilder helpMessage = new StringBuilder()
+				.append("In order to run the Unique View Time Calculator, call the main method with the argument 'input'.")
+				.append(System.lineSeparator())
+				.append("You will be prompted for a list of view times entered as a space-delimited list where the first")
+				.append(System.lineSeparator())
+				.append("number is the timestamp of when the view STARTED, the second number is the timestamp of when the")
+				.append(System.lineSeparator())
+				.append("view ENDED, the next number is a START timestamp, the next number is an END timestamp, etc.");
+			System.out.println(helpMessage);
 		} else if (args[0].equals("input")) {
 			Console console = System.console();
 			String timeSegmentString = console.readLine("Input view time segments: ");
 			UVTAlgorithm.uniqueViewTimeCalculator(timeSegmentString);
 		} else {
-			// insert text stating the only options are "help" or "input"
+			System.out.println("Unknown option specified. The only available commands are 'help' and 'input'.");
 		}
 	}
 }
